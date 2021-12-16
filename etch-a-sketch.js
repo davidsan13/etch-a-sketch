1e0b392c4570b0b4ce8576bce83c3974499d80ec
davidsa13 <davidsan@csu.fullerton.edu>
2021-12-15 21:12:29 -0800

    update grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,28 +1,24 @@
 
-for(let row = 0; row < 16; row++) {
-    const container = document.querySelector('#container');
-    const newDiv = document.createElement("div");
-    newDiv.classList.add('div'+ row)
-    newDiv.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
-    container.appendChild(newDiv);
-    for(let column = 0; column < 16; column++) {
-        const container = document.querySelector(".div" + row);
-        const newDiv = document.createElement("div");
-        newDiv.classList.add('column' + column)
-        newDiv.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
-        container.appendChild(newDiv);
 
-        const grid = document.querySelectorAll('.column' + column);
-        console.log(grid)
-        /* grid.forEach(function(gr) {
+init();
+createResetButton();
+
+function init(){
+    for(let row = 0; row < 16* 16; row++) {
+        const container = document.querySelector('.sub-container');
+        container.style.gridTemplateColumns= `repeat(16, 1fr)`
+        const newDiv = document.createElement("div");
+        newDiv.classList.add('div'+ row)
+        container.appendChild(newDiv);
+        const grid = document.querySelectorAll('.div' + row);
+        grid.forEach(function(gr) {
             gr.addEventListener("mouseover", hover);
-        })*/
+        });
     }
 }
-createResetButton()
 
 function createResetButton(){
-    const container = document.querySelector('#container');
+    const container = document.querySelector('.sub-container');
     const div = document.createElement('BUTTON');
     div.innerHTML = "RESET"
     div.classList.add('reset')
@@ -32,39 +28,41 @@ function createResetButton(){
 }
 
 function createGrid() {
-    const container = document.querySelector('#container');
-    container.style.setProperty('grid-template-columns: repeat('userInput',25px [col-start])')
     let userInput = prompt("Entergrid size");
-    let div = document.getElementById('container');
+    
+    const div = document.getElementsByClassName('sub-container');
+    console.log(div);
+    console.log(div.firstChild);
     while(div.firstChild){
         div.removeChild(div.firstChild);
+        console.log(hi)
     }
-    for(let row = 0; row < userInput; row++) {
+    /*for(let row = 0; row < userInput * userInput; row++) {
 
-        const container = document.querySelector('#container');
+        const container = document.querySelector('.sub-container');
+        container.style.gridTemplateColumns= `repeat(${userInput}, 1fr)`
         const newDiv = document.createElement("div");
         newDiv.classList.add('div'+ row)
-        container.appendChild(newDiv);
-        for(let column = 0; column < userInput; column++) {
+        container.appendChild(newDiv);*/
+        /* for(let column = 0; column < userInput; column++) {
             const container = document.querySelector(".div" + row);
             const newDiv = document.createElement("div");
             const newContent = document.createTextNode("hi");
             newDiv.appendChild(newContent)
             newDiv.classList.add('column' + column)
             container.appendChild(newDiv);
-            const grid = document.querySelectorAll('.column' + column);
+            
             console.log(grid)
-            /*grid.forEach(function(gr) {
+            grid.forEach(function(gr) {
                 gr.addEventListener("mouseover", hover);
-            })*/
-        }
-    }
+            })
+        }*/
+    
 }
 
-/* function hover() {
+function hover() {
     this.setAttribute("style", "background-color: black;")
-}*/
+}
 
 function clearGrid() {
-
 }
